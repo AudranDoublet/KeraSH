c2ab3393bdd538f7e5b7f0ee61690966d1b0d97d
Jean-Adrien DUCASTAING <jean-adrien.ducastaing@epita.fr>
2019-04-05 19:14:19 +0200

    arch: minor changes

--- src/util/data.zsh
+++ src/util/data.zsh
@@ -6,22 +6,34 @@ generate_data()
     local file_y=$2
     local model=$3
 
+    local x_train_path="${MODEL}/${model}/x_train.dat"
+    local x_val_path="${MODEL}/${model}/x_val.dat"
+    local y_train_path="${MODEL}/${model}/y_train.dat"
+    local y_val_path="${MODEL}/${model}/y_val.dat"
+
     local sample_size=$(wc -l ${file_x} | cut -d ' ' -f 1)
     local train_samples=$(( int(rint(sample_size * 0.8)) ))
     local sample_id=0
 
-    while IFS="," read line;
+    setopt shwordsplit
+
+    while read line;
     do
+        IFS=,
         v=($line)
         if (( ${sample_id} < ${train_samples} ))
         then
-            for value in "${line[@]}"; do
-                print "${value} " >> "${MODEL}/${model}/x_train.dat"
+            for x in $v;
+            do
+                printf "$x " >> "${x_train_path}"
             done
+            printf "\n" >> "${x_train_path}"
         else
-            for value in "${line[@]}"; do
-                print "${value}" >> "${MODEL}/${model}/x_val.dat"
+            for x in $v;
+            do
+                printf "$x " >> "${x_val_path}"
             done
+            printf "\n" >> "${x_val_path}"
         fi
 
         sample_id=$(( sample_id + 1 ))
@@ -29,18 +41,23 @@ generate_data()
     done < $file_x
 
     sample_id=0
-    while IFS="," read line;
+    while read line;
     do
+        IFS=,
         v=($line)
         if (( ${sample_id} < ${train_samples} ))
         then
-            for value in "${v[@]}"; do
-                print "${value} " >> "${MODEL}/${model}/y_train.dat"
+            for y in $v;
+            do
+                printf "$y " >> "${y_train_path}"
             done
+            print "\n" >> "${y_train_path}"
         else
-            for value in "${v[@]}"; do
-                print "${value}" >> "${MODEL}/${model}/y_val.dat"
+            for y in $v;
+            do
+                printf "$y " >> "${y_val_path}"
             done
+            printf "\n" >> ${y_val_path}
         fi
 
         sample_id=$(( sample_id + 1 ))
