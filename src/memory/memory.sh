d3f9cc9e82fab291c8391ac372295ef274b9e2c3
Jean-Adrien DUCASTAING <jean-adrien.ducastaing@epita.fr>
2019-04-05 14:31:18 +0200

    [MEMORY] Add model dir in arch

--- src/memory/memory.sh
+++ src/memory/memory.sh
@@ -1,11 +1,13 @@
 MOUNTPOINT="./kerash_mountpoint"
 MAT="./kerash_mountpoint/mat"
+MODEL="./kerash_mountpoint/models/"
 
 init_fs()
 {
     mkdir -p "${MOUNTPOINT}"
     sudo mount -t tmpfs -o size="${1}m" tmpfs "${MOUNTPOINT}"
     mkdir -p "${MAT}"
+    mkdir -p "${MODEL}"
 }
 
 end_fs()
