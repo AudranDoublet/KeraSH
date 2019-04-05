68e01c264321233cb5ab6b13b0c816ec4815fd39
Audran Doublet <audran.doublet@epita.fr>
2019-04-05 20:21:50 +0200

    blas: matrix: implement matrix_sub

--- src/blas/matrix_operations.zsh
+++ src/blas/matrix_operations.zsh
@@ -23,6 +23,28 @@ function matrix_add()
     done
 }
 
+#   Usage
+#
+# print sum of FD:3 and FD:4 matrix in stdout
+function matrix_sub()
+{
+    matrix_load w1 h1 a1 <&3
+    matrix_load w2 h2 a2 <&4
+
+    if (( w1 != w2 || h1 != h2 ));
+    then
+        echo "matrix_sub: matrices have different size" >&2
+        return 1
+    fi
+
+    size=$((w1 * h1))
+    echo $w1 $h1
+
+    for ((i = 1; i <= size; i++));
+    do
+        echo $((a1[i] - a2[i]))
+    done
+}
 
 #   Usage
 #
