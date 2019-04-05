7f5a4e802e1cad1cf6048ebbeacaccd387cab032
Audran Doublet <audran.doublet@epita.fr>
2019-04-05 19:01:04 +0200

    blas: matrix: implement matrix_transpose

--- src/blas/matrix_operations.zsh
+++ src/blas/matrix_operations.zsh
@@ -22,3 +22,22 @@ function matrix_add()
         echo $((a1[i] + a2[i]))
     done
 }
+
+
+#   Usage
+#
+# print transposed STDIN matrix in STDOUT
+function matrix_transpose()
+{
+    matrix_load w1 h1 a1
+
+    echo $h1 $w1
+
+    for ((x = 0; x < w1; x++));
+    do
+        for ((y = 0; y < h1; y++));
+        do
+            echo ${a1[x + y * size + 1]}
+        done
+    done
+}
