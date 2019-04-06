46dd27f97b56a61a54ef91b982d8c2891b8f01e9
Jean-Adrien DUCASTAING <jean-adrien.ducastaing@epita.fr>
2019-04-06 12:11:46 +0200

    arch: moved matrix uid generator func to util/organization

--- src/training/cost.zsh
+++ src/training/cost.zsh
@@ -1,13 +1,4 @@
-function matrix_namer()
-{
-    local genotype_name="${1}"
-    local epoch_nb="${2}"
-    local sample_id="${3}"
-    local layer_id="${4}"
-    local mat_type="${5}"
-
-    echo "${genotype_name}_${epoch_nb}_${layer_id}_${sample_id}_${mat_type}.dat"
-}
+source ./util/organization.zsh
 
 function cost_mse()
 {
