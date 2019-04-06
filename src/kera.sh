9d7bd79ca14e3efa7627e34db9b1a76751d17ae9
Audran Doublet <audran.doublet@epita.fr>
2019-04-06 20:04:02 +0200

    kerash: import training

--- src/kera.sh
+++ src/kera.sh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 
+zmodload zsh/clone
+
 source ./memory/memory.zsh
 source ./blas/import.zsh
+source ./training/import.zsh
 source ./util/data.zsh
 source ./util/organization.zsh
---
+++ src/training/import.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+source ./training/activation.zsh
+source ./training/cost.zsh
+source ./training/forward_propagation.zsh
+source ./training/backprop.zsh
+source ./training/genetic.zsh
