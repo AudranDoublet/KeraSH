5c7afaf79938042f12dab672fc8232378ca83b68
Jean-Adrien DUCASTAING <jean-adrien.ducastaing@epita.fr>
2019-04-06 19:01:56 +0200

    training: fit: implement fit_batch

--- src/training/backprop.zsh
+++ src/training/backprop.zsh
@@ -60,3 +60,82 @@ function fit_sample()
         fit_"$layer_type" "$genome_dir/topology/layer_$i" "$activation" "$i"
     done
 }
+
+function fit_batch_part()
+{
+    gen_id=$1
+    batch_size=$2
+    first_feature=$3
+    last_feature=$4
+
+    for (( i = first_feature; i < last_feature; i++ ))
+    do
+        fit_sample "${MODEL}/x_train/sample_$i.dat" "${MODEL}/y_train/sample_$i.dat"
+    done
+}
+
+#   Usage
+#
+# Backpropagation for a batch
+# Parameters:
+#   genome id
+#   batch size
+#   number of processor
+#   first feature
+#   last feature
+#   learniong rate
+function fit_batch()
+{
+    local gen_id=$1
+    local batch_size=$2
+    local nb_proc=$3
+    local first_feature=$4
+    local last_feature=$((first_feature + batch_size))
+    local learning_rate=$5
+
+    local genome_dir="${MODEL}/genomes/gen_${$1}"
+
+    # Get model's layer count
+    matrix_load _ _ metadata < "${genome_dir}/meta.dat"
+    local nb_layer=${metadata[3]}
+
+    # Fork nb_proc times in order to train a part of the batch
+    pids=()
+
+    for (( i = 0; i < nb_proc; i++ ));
+    do
+        beg=$((i * batch_size / nb_proc))
+        en=$(((i + 1) * batch_size / nb_proc - 1))
+
+        clone $TTY 2> /dev/null >&2
+
+        if (( $! == 0 ));
+        then
+            fit_batch_part "$gen_id" "$batch_size" "$beg" "$en"
+            exit 0
+        fi
+
+        pids[$(i + 1)]=$!
+    done
+
+    # Wait for all process
+    wait $pids
+
+    # Sum all gradients from all process for each layer
+    for pid in $pids;
+    do
+        for (( i = 0; i < nb_layers; i++ ));
+        do
+            matrix_add_inplace "$(predict_name $i gradients)" "${MAT}/$pid/${i}_gradients.dat"
+        done
+    done
+
+    # Apply learning rate/gradient on each layer
+    for (( i = 0; i < nb_layers; i++ ));
+    do
+        matrix_mul_scalar $((1.0 * learning_rate / batch_size)) \
+                < "$(predict_name $i gradients)" > "$(tmp_file 0)"
+
+        matrix_add_inplace "$genome_dir/topology/layer_$i/weights.dat" "$(tmp_file 0)"
+    done
+}
