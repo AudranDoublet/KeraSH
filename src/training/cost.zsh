88af0b6c4909bb20408b19262972321604c87058
Jean-Adrien DUCASTAING <jean-adrien.ducastaing@epita.fr>
2019-04-06 10:52:40 +0200

    training: cost: implement mse cost function

---
+++ src/training/cost.zsh
@@ -0,0 +1,24 @@
+function matrix_namer()
+{
+    local genotype_name="${1}"
+    local epoch_nb="${2}"
+    local sample_id="${3}"
+    local layer_id="${4}"
+    local mat_type="${5}"
+
+    echo "${genotype_name}_${epoch_nb}_${layer_id}_${sample_id}_${mat_type}.mat"
+}
+
+function mse()
+{
+    # Formula: 1/m*(Y-X)^2
+    local x_path="${1}"
+    local y_path="${2}"
+    local batch_size=$3
+    local temp1_name=(matrix_namer "${4}" $5 $6 $7 "cost_temp1")
+    local temp2_name=(matrix_namer "${4}" $5 $6 $7 "cost_temp2")
+    local output_name=(matrix_namer "${4}" $5 $6 $7 "cost")
+    matrix_sub > "${MAT}/${temp1_name}" 3< "${y_path}" 4< "${x_path}"
+    matrix_mul_self_p2p < "${MAT}/${temp2_name}" > "${MAT}/${temp1_name}"
+    matrix_mul_scalar $(( 1 / batch_size )) < "${MAT}/${temp2_name}" > "${MAT}/output_name"
+}
