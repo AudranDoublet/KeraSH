cc027657c1e455e77007da62eeda07dfd035195d
Jean-Adrien DUCASTAING <jean-adrien.ducastaing@epita.fr>
2019-04-06 18:57:35 +0200

    training: cost: constistancy of cost_mse

--- src/training/cost.zsh
+++ src/training/cost.zsh
@@ -6,12 +6,11 @@ function cost_mse()
     local x_path="${1}"
     local y_path="${2}"
     local batch_size=$3
-    local temp1_name=$(matrix_namer "${4}" $5 $6 $7 "cost_temp1")
-    local temp2_name=$(matrix_namer "${4}" $5 $6 $7 "cost_temp2")
-    local output_name=$(matrix_namer "${4}" $5 $6 $7 "cost")
-    matrix_sub > "${MAT}/${temp1_name}" 3< "${y_path}" 4< "${x_path}"
-    matrix_mul_self_p2p < "${MAT}/${temp1_name}" > "${MAT}/${temp2_name}"
-    matrix_mul_scalar $(( 1.0 / $batch_size )) < "${MAT}/${temp2_name}" > "${MAT}/${output_name}"
 
-    echo "${MAT}/${output_name}"
+    local temp1_name=$(tmp_name 1)
+    local temp2_name=$(tmp_name 2)
+
+    matrix_sub > "${temp1_name}" 3< "${y_path}" 4< "${x_path}"
+    matrix_mul_self_p2p < "${temp1_name}" > "${temp2_name}"
+    matrix_mul_scalar $(( 1.0 / $batch_size )) < "${temp2_name}"
 }
