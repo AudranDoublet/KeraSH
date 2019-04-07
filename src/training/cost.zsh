56259c12bc48b01c3f665bdc44a6b02573e91d02
Jean-Adrien DUCASTAING <jean-adrien.ducastaing@epita.fr>
2019-04-07 21:50:02 +0200

    cost: add cost derivative

--- src/training/cost.zsh
+++ src/training/cost.zsh
@@ -2,7 +2,7 @@ source ./util/organization.zsh
 
 function cost_mse()
 {
-    # Formula: 1/m*(Y-X)^2
+    # Formula: 1/m*(X-Y)^2
     local x_path="${1}"
     local y_path="${2}"
     local batch_size=$3
@@ -10,7 +10,19 @@ function cost_mse()
     local temp1_name=$(tmp_name 1)
     local temp2_name=$(tmp_name 2)
 
-    matrix_sub > "${temp1_name}" 3< "${y_path}" 4< "${x_path}"
+    matrix_sub > "${temp1_name}" 4< "${y_path}" 3< "${x_path}"
     matrix_mul_self_p2p < "${temp1_name}" > "${temp2_name}"
     matrix_mul_scalar $(( 1.0 / $batch_size )) < "${temp2_name}"
+}
+
+function cost_d_mse()
+{
+    # Formula (X-Y)
+    local x_path="${1}"
+    local y_path="${2}"
+    local batch_size=$3
+
+local temp1_name=$(tmp_name 1)
+
+    matrix_sub > "${temp1_name}" 4< "${y_path}" 3< "${x_path}"
 }
