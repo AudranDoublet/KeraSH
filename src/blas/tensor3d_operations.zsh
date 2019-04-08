f8a1c4834ab623e8a5020cf64670946552b51ae0
Audran Doublet <audran.doublet@epita.fr>
2019-04-08 19:24:57 +0200

    blas: tensor: implement tensor_apply_convolution

--- src/blas/tensor3d_operations.zsh
+++ src/blas/tensor3d_operations.zsh
@@ -10,7 +10,7 @@
 #   width
 #   height
 #   stride
-#   padding
+#   pad
 function tensor_splice()
 {
     local d=$1
@@ -19,7 +19,7 @@ function tensor_splice()
     local e_x=$((s_x + $4))
     local e_y=$((s_y + $5))
     local stride=$6
-    local padding=$7
+    local pad=$7
 
     tensor_load w h d a
 
@@ -43,6 +43,39 @@ function tensor_splice()
             else
                 echo ${a[x + y*w + d*w*h + 1]}
             fi
+        done
+    done
+}
+
+function tensor_apply_convolution()
+{
+    local splice=$1
+    local pad=$2
+
+    local d=0
+    local x=0
+    local y=0
+
+    tensor_load w1 h1 d1 a1 <&3
+    tensor_load w2 h2 d2 a2 <&3
+
+    if (( d1 != d2 ));
+    then
+        echo "tensor_apply_convolution: different depth"
+        return 1
+    fi
+
+    for (( d = 0; d < d1; d++ ));
+    do
+        tensor_splice 0 0 $w2 $h2 1 0 > $(tmp_name 1)
+
+        for (( y = 0; y < h1; y++ ));
+        do
+            for (( x = 0; x < w1; x++ ));
+            do
+                tensor_splice x y $w2 $h2 "$splice" "$pad" > $(tmp_name 2)
+                matrix_conv 3< $(tmp_name 1) 4< $(tmp_name 2)
+            done
         done
     done
 }
