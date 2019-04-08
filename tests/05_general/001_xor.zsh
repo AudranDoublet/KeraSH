d4eacb62167b427f3988f24d579534b9a4c7fd6c
Audran Doublet <audran.doublet@epita.fr>
2019-04-08 13:54:55 +0200

    tests: add test for matrix_similar

--- tests/05_general/001_xor.zsh
+++ tests/05_general/001_xor.zsh
@@ -9,7 +9,7 @@ init_fs
 store_model "xor" ../test_data/test_model ../test_data/test_data ../test_data/test_label
 create_genome "xor" "${MODEL}/xor.model"
 
-fit 10 "xor" 1 1 0.001
+fit 1000 "xor" 4 4 10
 
 # Backup data and end file system
 #mkdir -p "./kerash_data"
