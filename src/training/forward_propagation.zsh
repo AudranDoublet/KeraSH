f98fea3157e5926c12c4e86518d31ce72ac7f59f
Audran Doublet <audran.doublet@epita.fr>
2019-04-08 20:54:39 +0200

    training: forward: add forward_convolution

--- src/training/forward_propagation.zsh
+++ src/training/forward_propagation.zsh
@@ -18,6 +18,46 @@ function predict_dense()
     input_file="$(predict_name $layerid activation)"
 }
 
+function predict_convolution()
+{
+    local dir="$1"
+    local activation="$2"
+    local layerid="$3"
+    local filter_count="$4"
+    local stride="$5"
+    local pad="$6"
+    local kernel_w="$7"
+    local kernel_h="$8"
+
+    local i
+
+    read w1 h1 d1 < "${input_file}"
+
+    if (( pad == 0 ));
+    then
+        w1=$((w1 - w1 % kernel_w))
+        h1=$((h1 - h1 % kernel_h))
+    fi
+
+    w1=$(( (w1 - kernel_w + 2 * pad) / stride + 1 ))
+    h1=$(( (h1 - kernel_h + 2 * pad) / stride + 1 ))
+    d1=$(( d1 * filter_count ))
+
+    {
+        echo $w1 $h1 $d1
+
+        for ((i = 0; i < filter_count; i++));
+        do
+            tensor_apply_convolution $stride $pad $w1 $h1\
+                    3< "${input_file}" 4< "${dir}/filter_$i.dat"
+        done
+    } > "$(predict_name $laerid activity)"
+
+    input_file="$(predict_name $layerid activation)"
+    tensor_apply activ_$activation < "$(predict_name $layerid activity)" \
+                                   > "${input_file}"
+}
+
 function predict_output()
 {
     cp "$input_file" "$(predict_name 0 output)"
@@ -42,7 +82,7 @@ function predict()
 
     for ((i = 0; i < nb_layer; i++))
     do
-        read activation layer_type < "$genome_dir/topology/layer_$i/meta.dat"
-        predict_"$layer_type" "$genome_dir/topology/layer_$i" "$activation" "$i"
+        read activation layer_type custom < "$genome_dir/topology/layer_$i/meta.dat"
+        predict_"$layer_type" "$genome_dir/topology/layer_$i" "$activation" "$i" $custom
     done
 }
